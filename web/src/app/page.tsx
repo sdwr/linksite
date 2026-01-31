24699c55c3650fba67789611cf5ed54ef8451275
Rory <rorykaufmann@gmail.com>
2026-01-31 05:43:59 +0000

    link page: title-as-link, domain filter, star top-right, inline tags; / redirects to /add

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,21 +1,5 @@
-import { LinkView } from '@/components/LinkView';
-import { MockProvider } from '@/providers/MockProvider';
-import { RealProvider } from '@/providers/RealProvider';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const useRealBackend = process.env.NEXT_PUBLIC_USE_REAL_BACKEND === 'true';
-
-  if (useRealBackend) {
-    return (
-      <RealProvider>
-        <LinkView />
-      </RealProvider>
-    );
-  }
-
-  return (
-    <MockProvider>
-      <LinkView />
-    </MockProvider>
-  );
+  redirect('/add');
 }
